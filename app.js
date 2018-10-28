757c5b4310ba7e66b459ee050aad30e3721366a6
Danee187 <kisdani0720@gmail.com>
2018-10-28 19:41:37 +0100

    Alapok atvetele 1.

---
+++ app.js
@@ -0,0 +1,19 @@
+//    Pluralsight - Javascript Első léőések 
+
+/*
+let  productName = "hammer\' s",
+     productId = "H123";
+
+console.log(productName,productId)
+*/
+
+
+//--------------- Black Jack -------------------
+
+let card1 = "Ace of Spades",
+    card2 = "Ten of Hearts";
+
+    console.log("Welcome to Blackjack Game!");
+    console.log("You are dealt: ");
+    console.log(" "+ card1);
+    console.log(" "+ card2);
